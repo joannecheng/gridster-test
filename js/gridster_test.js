85f5d9f57eee0a2319f96062a5cff2509bb368c2
jc <chengjoanne2@gmail.com>
2012-11-01 17:35:33 -0600

    update javascript

--- js/gridster_test.js
+++ js/gridster_test.js
@@ -10,13 +10,9 @@ $(function(){
     var gridster = grid.data('gridster');
     var numCols = 7;
     var windowWidth = $(window).width();
-    console
     var baseDimension = (windowWidth - numCols*10)/numCols;
     
-    gridster.options.widget_base_dimensions = [baseDimension, baseDimension];
-    gridster.generate_grid_and_stylesheet();
-    gridster.get_widgets_from_DOM();
-    gridster.set_dom_grid_height();
+    gridster.resize_widget_dimensions({widget_base_dimensions: [baseDimension, baseDimension]});
   });
 
 });
