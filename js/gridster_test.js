a4bac31c255dcafedd2479c5cdfb5ee57ccf23b3
Joanne Cheng <chengjoanne2@gmail.com>
2012-11-13 13:36:37 -0700

    handle resizing

--- js/gridster_test.js
+++ js/gridster_test.js
@@ -2,15 +2,16 @@ $(function(){
   var grid;
   
   grid = $('.gridster ul').gridster({
+    max_size_x: 4,
     widget_margins: [10, 10],
     widget_base_dimensions: [140, 140]
   });
 
   $(window).resize( function() {
     var gridster = grid.data('gridster');
-    var numCols = 7;
     var windowWidth = $(window).width();
-    var baseDimension = (windowWidth - numCols*10)/numCols;
+    var numCols = 8;
+    var baseDimension = (windowWidth-(numCols+2)*10)/numCols;
     
     gridster.resize_widget_dimensions({widget_base_dimensions: [baseDimension, baseDimension]});
   });
